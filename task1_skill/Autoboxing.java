999379e5c78b3e56121bc227bcc43c12a576ca5d
Srikar-111 <2420030111@klh.edu.in>
2025-07-24 11:41:03 +0530

    Task1 Skill

---
+++ task1_skill/Autoboxing.java
@@ -0,0 +1,18 @@
+package task1_skill;
+
+public class Autoboxing {
+
+	public static void main(String[] args) {
+		
+		int number = 27;
+		
+		Integer wrapped_num = Integer.valueOf(number); //converting explicitly 
+		Integer converted_num = number; //converting using autoboxing
+		
+		System.out.println("Interger : "+ number);
+		System.out.println("Explicit Conversion :" + wrapped_num);
+		System.out.println("Autoboxing to integer : "+ converted_num);
+	
+	}
+
+}
